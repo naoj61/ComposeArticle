94ef9d6561c61bad24dbd2991f8778f87f9fe401
naoj61 <jppandroid@gmail.com>
2025-11-29 19:47:17 +0100

    Acabat

--- app/src/main/java/com/example/composearticle/MainActivity.kt
+++ app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -5,19 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -31,65 +27,68 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-           // HappyBirthdayTheme {
+            ComposeArticleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    com.example.composearticle.GreetingImage(
-                        message = "Missatge")
+                    ComposeArticleApp()
                 }
-           // }
+            }
         }
     }
 }
 
 @Composable
-fun GreetingText(message: String, modifier: Modifier = Modifier) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
-    ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center,
-        )
-        Text(
-            text = "xx",
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(end = 16.dp)
-                .align(alignment = Alignment.CenterHorizontally)
-        )
-    }
+fun ComposeArticleApp() {
+    ComposeArticleText(
+        stringResource(R.string.titol_text),
+        stringResource(R.string.sub_titol_text),
+        stringResource(R.string.cos_text),
+        painterResource(R.drawable.bg_compose_background)
+    )
 }
 
 @Composable
-fun GreetingImage(message: String, modifier: Modifier = Modifier){
-    val image = painterResource(R.drawable.bg_compose_background)
-    Box(modifier) {
+fun ComposeArticleText(titol: String, subTitol: String, cos: String, image: Painter
+                       , modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+    ) {
         Image(
             painter = image,
             contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F
+            alpha = 1F
         )
-        GreetingText(
-            message = message,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
+
+        Text(
+            text = titol,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+                //.align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = subTitol,
+            fontSize = 16.sp,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp)
+          //      .align(alignment = Alignment.CenterHorizontally)
+        )
+        Text(
+            text = cos,
+            fontSize = 16.sp,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+               // .align(alignment = Alignment.CenterHorizontally)
         )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ComposeArticlePreview() {
     ComposeArticleTheme {
-        GreetingImage("Android")
+        ComposeArticleApp()
     }
 }
